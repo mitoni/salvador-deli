57d8a105e4b003dc115ec0e55f40a6430fca8e11
michele-tonizzo <mt@moi.am>
2024-03-01 16:16:45 -0500

    fix(backend): add static paths

--- src/pages/[[...slug]].tsx
+++ src/pages/[[...slug]].tsx
@@ -1,8 +1,4 @@
-import {
-  GetStaticPathsContext,
-  GetStaticPropsContext,
-  InferGetStaticPropsType,
-} from "next";
+import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import { client } from "../../tina/__generated__/client";
 import { useTina } from "tinacms/dist/react";
 import { Page } from "../components/layouts/page/Page";
@@ -11,6 +7,7 @@ import Menu from "../components/sections/menu/Menu";
 export default function Slug(
   props: InferGetStaticPropsType<typeof getStaticProps>
 ) {
+  console.log({ props });
   const { data } = useTina(props);
 
   return (
@@ -21,11 +18,15 @@ export default function Slug(
   );
 }
 
-export async function getStaticPaths(context: GetStaticPathsContext) {
-  //!TODO
+export async function getStaticPaths() {
+  const connection = await client.queries.pageConnection();
+  const paths = connection.data.pageConnection.edges?.map((page) => {
+    return page?.node?.url;
+  });
+
   return {
-    paths: [],
-    fallback: false,
+    paths,
+    fallback: "blocking",
   };
 }
 
