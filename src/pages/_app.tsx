8d3689a4b5b8a4bcbec71b7e55d00a936223589e
michele-tonizzo <mt@moi.am>
2024-03-07 18:47:18 -0500

    feat(analytics): add title and description tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "../styles/theme";
 import React from "react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   React.useEffect(() => {
@@ -14,6 +15,19 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <MantineProvider theme={theme}>
+      <Head>
+        <title key="title">Salvador Deli</title>
+        <meta property="og:title" content="Salvador Deli" key="og:title" />
+
+        <meta name="description" key="description">
+          Artfully Elevated Pub Food in Vero Beach, FL
+        </meta>
+        <meta
+          property="og:description"
+          content="Artfully Elevated Pub Food in Vero Beach, FL"
+          key="og:description"
+        />
+      </Head>
       <Component {...pageProps} />
     </MantineProvider>
   );
