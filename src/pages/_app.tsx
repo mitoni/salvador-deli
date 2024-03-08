9c858b302de0da0ba85dc5a896a8ab51181cdc7b
michele-tonizzo <mt@moi.am>
2024-03-08 14:25:24 -0500

    fix(ui): remove title and description causing error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,6 @@ import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "../styles/theme";
 import React from "react";
-import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   React.useEffect(() => {
@@ -15,19 +14,6 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <MantineProvider theme={theme}>
-      <Head>
-        <title key="title">Salvador Deli</title>
-        <meta property="og:title" content="Salvador Deli" key="og:title" />
-
-        <meta name="description" key="description">
-          Artfully Elevated Pub Food in Vero Beach, FL
-        </meta>
-        <meta
-          property="og:description"
-          content="Artfully Elevated Pub Food in Vero Beach, FL"
-          key="og:description"
-        />
-      </Head>
       <Component {...pageProps} />
     </MantineProvider>
   );
